553d88028eb792782462b1125aa6f169baeef749
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 20:38:51 -0300

    fix clips

--- go.sh
+++ go.sh
@@ -7,9 +7,9 @@ pactl unload-module ladspa-sink;
 [0:a]anlmdn=s=30,\
 equalizer=f=800:width_type=h:width=100:g=-3,\
 deesser=f=1.0,\
-ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=444 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0.05 1.0 1.0,aecho=0.8:0.9:1000:0.3,speechnorm=e=8:r=0.0001:l=1[avoc];\
+ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=444 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0.05 1.0 1.0,aecho=0.8:0.9:1000:0.3,speechnorm=e=6:r=0.0001:l=1[avoc];\
 [avoc][1:a]amix=inputs=2:weights=0.7|0.3[amixed];\
-[amixed]compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7[w]\
+[amixed]compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15[w]\
 " -map "[w]" ${1}_go.mp3 -y;
 
 echo  "PLAY!";
