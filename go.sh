8e11f42f83dada5123f28c50539b00d82d752b41
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 22:57:59 -0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -5,8 +5,8 @@ pactl unload-module ladspa-sink;
 
 	ffmpeg -i ${1}_voc.wav -i ${1}.wav -filter_complex \
 "[0:a]anlmdn=s=30,\
-ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=440.000 0.000 0.000 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 1.000 1.00 0.000 0 0.000 0.000 0.000 0.000 0.000 1.000,\
-deesser=f=0.1,\
+ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=440.000 0.000 0.000 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 0.5 1.00 0.000 0 0.000 0.000 0.000 0.000 0.000 1.000,\
+deesser=f=0.05,\
 dynaudnorm,\
 speechnorm,\
 compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15,\
@@ -18,8 +18,9 @@ alimiter=level_in=0.5:attack=0.1:release=1,\
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,\
 aresample=resampler=soxr:osf=s16[avoc];\
 [1:a]aresample=resampler=soxr:osf=s16[a1];\
-[avoc][a1]amix=inputs=2:weights=0.8|0.2;"\
+[avoc][a1]amix=inputs=2:weights=1.0|0.25;"\
  ${1}_go.mp3 -y;
+
 
 
 echo  "PLAY!";
