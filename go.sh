291f10742d6d9127263932f883721583174b8e07
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 21:36:54 -0300

    ressample

--- go.sh
+++ go.sh
@@ -3,20 +3,18 @@
 pactl unload-module module-loopback;
 pactl unload-module ladspa-sink;
 
-	ffmpeg -loglevel info -i ${1}_voc.wav -i ${1}.wav -filter_complex "\
-[0:a]anlmdn=s=30,\
+	ffmpeg -loglevel info -i ${1}_voc.wav -i ${1}.wav -filter_complex \
+"[0:a]anlmdn=s=30,\
 equalizer=f=800:width_type=h:width=100:g=-3,\
 ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=444 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0.05 1.0 1.0,\
 deesser=f=0.25,\
 aecho=0.5:0.6:100:0.3,\
 speechnorm=e=6:r=0.0001:l=1,\
 compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15[avoc];\
-[1:a]volume=volume=-20dB[a1];\
-[avoc]volume=volume=-15dB[a2];\
-[a1][a2]amix=inputs=2:weights=1|1;\
-" ${1}_go.mp3 -y;
-
-
+[1:a]aresample=resampler=soxr:osf=s16:aos=48000,volume=volume=-5dB[a1];\
+[avoc]aresample=resampler=soxr:osf=s16:aos=48000,volume=volume=0dB[a2];\
+[a1][a2]amix=inputs=2:weights=0.3|0.6;"\
+ ${1}_go.mp3 -y;
 
 echo  "PLAY!";
 mplayer ${1}_go.mp3;
