1395dc5b0eb80b7dbd0ccad25384d2405eea48f4
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 22:19:30 -0300

    remove LFO

--- go.sh
+++ go.sh
@@ -5,7 +5,7 @@ pactl unload-module ladspa-sink;
 
 	ffmpeg -i ${1}_voc.wav -i ${1}.wav -filter_complex \
 "[0:a]anlmdn=s=10,\
-ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=440.000 0.000 0.000 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 1.000 1.000 0.000 0 0.000 5.412 1.000 1.000 0.000 1.00,\
+ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=440.000 0.000 0.000 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 1.000 1.000 0.000 0 0.000 0.000 1.000 1.000 0.000 1.00,\
 deesser=f=0.1,\
 dynaudnorm,\
 speechnorm,\
@@ -19,13 +19,8 @@ alimiter=level_in=0.5:attack=0.1:release=1,\
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,\
 aresample=resampler=soxr:osf=s16,volume=volume=0.2dB[avoc];\
 [1:a]aresample=resampler=soxr:osf=s16,volume=volume=-2dB[a1];\
-[avoc][a1]amix=inputs=2:weights=0.7|0.3;"\
+[avoc][a1]amix=inputs=2:weights=0.6|0.8;"\
  ${1}_go.mp3 -y;
-
-
-
-
-
 
 
 echo  "PLAY!";
