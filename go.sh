bb037878ea454490ec5580021101ae1f25ea4696
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 21:07:14 -0300

    remove  split errado

--- go.sh
+++ go.sh
@@ -11,12 +11,9 @@ deesser=f=0.25,\
 aecho=0.5:0.6:100:0.3,\
 speechnorm=e=6:r=0.0001:l=1,\
 compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15[avoc];\
-[1:a]asplit=2[bg][fg];\
-[fg][avoc]sidechaincompress=threshold=0.5:ratio=5:attack=0.1:release=0.1[side];\
-[bg][side]amix=inputs=2[audio]\
+[1:a][avoc]sidechaincompress=threshold=0.5:ratio=5:attack=0.1:release=0.1[side];\
+[1:a][side]amix=inputs=2[audio]\
 " -map "[audio]" ${1}_go.mp3 -y;
-
-
 
 echo  "PLAY!";
 mplayer ${1}_go.mp3;
