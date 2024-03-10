cbdebe8326ff4942a1778f2f0c0ccf63ccee380c
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 23:44:34 -0300

    adj ECHO lower deesser

--- go.sh
+++ go.sh
@@ -6,11 +6,11 @@ pactl unload-module ladspa-sink;
 	ffmpeg -i ${1}_voc.wav -i ${1}.wav -filter_complex \
 "[0:a]anlmdn=s=25,\
 equalizer=f=800:width_type=h:width=100:g=-6,\
-deesser=f=0.95,\
+deesser=f=0.05,\
 ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=440 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 1.0000,\
 alimiter,\
 speechnorm=e=50:r=0.0001:l=1,\
-aecho=0.8:0.9:33:0.255,\
+aecho=0.8:0.9:69:0.255,\
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,\
 aresample=resampler=soxr:osf=s16[avoc];\
 [1:a]aresample=resampler=soxr:osf=s16[a1];\
