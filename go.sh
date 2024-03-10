acb1b497e6db97294d81e6eb827b57197784395f
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 21:02:36 -0300

    fix input links

--- go.sh
+++ go.sh
@@ -10,12 +10,12 @@ ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=444 0 0 1 1 1 1 1 1
 deesser=f=0.25,\
 aecho=0.5:0.6:100:0.3,\
 speechnorm=e=6:r=0.0001:l=1,\
-compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15\
-[avoc];\
-[1:a][avoc]asplit=2[bg][fg];\
-[fg][bg]sidechaincompress=threshold=0.5:ratio=5:attack=0.1:release=0.1[side];\
+compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15[avoc];\
+[1:a]asplit=2[bg][fg];\
+[fg][avoc]sidechaincompress=threshold=0.5:ratio=5:attack=0.1:release=0.1[side];\
 [bg][side]amix=inputs=2[audio]\
 " -map "[audio]" ${1}_go.mp3 -y;
+
 
 
 echo  "PLAY!";
