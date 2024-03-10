b7284f01d70aa4debdb32425a57b95fe05b9fcf5
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-09 21:43:36 -0300

    more standard autotalent params
    The autotalent plugin's parameters are set to more standard values, providing subtle pitch correction.
    The detection threshold is set to 0.5, which should provide gentle pitch correction without introducing excessive artifacts.
    Other filters remain similar to the previous command for normalization, equalization, de-essing, echo, and mixing.

--- go.sh
+++ go.sh
@@ -6,8 +6,8 @@ pactl unload-module ladspa-sink;
 	ffmpeg -loglevel info -i ${1}_voc.wav -i ${1}.wav -filter_complex \
 "[0:a]anlmdn=s=30,\
 equalizer=f=800:width_type=h:width=100:g=-3,\
-ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=444 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0.02 1.0 1.0,\
-deesser=f=0.25,\
+ladspa=/usr/lib/ladspa/tap_autotalent.so:plugin=autotalent:c=444 0 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0 0 0 0 0 0 1 1,\
+deesser=f=1,\
 aecho=0.5:0.6:100:0.3,\
 speechnorm=e=6:r=0.0001:l=1,\
 compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15[avoc];\
@@ -15,6 +15,7 @@ compand=points=-90/-90|-70/-70|-30/-15|0/-15|20/-15[avoc];\
 [avoc]aresample=resampler=soxr:osf=s16,volume=volume=0dB[a2];\
 [a1][a2]amix=inputs=2:weights=0.6|0.3;"\
  ${1}_go.mp3 -y;
+
 
 
 
