a26cedd17924aeebb30ff33c8778620f6f079f12
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-11 01:19:12 -0300

    v1.5

--- go.sh
+++ go.sh
@@ -3,32 +3,38 @@
 pactl unload-module module-loopback;
 pactl unload-module ladspa-sink;
 
-ffmpeg -hide_banner   -i ${1}_voc.wav -i ${1}.wav -filter_complex "\
-
-[0:a]adeclip,\
-anlmdn=s=1000,\
-dynaudnorm,\
-
-ladspa=tap_autotalent:plugin=autotalent:\
-c=440 0.0 0.000 \
-0 0 0 0 0 0 0 0 0 0 0 0 \
-1.00 1.000 0.000 0 \
-0.000 0 \
-1.000 1.000 0.000 \
-0.15,\
-
-compand=attacks=0:points=-80/-80|-12.4/-12.4|-6/-8|0/-6.8|20/-2.8,\
-aecho=0.8:0.9:75:0.255,\
+ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
+[0:a]
+adeclip,
+anlmdn=s=25,
+compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
+speechnorm=e=8:r=0.0001:l=1,
+ladspa=tap_autotalent:plugin=autotalent:
+c=440 0.0 0.000 
+1 0.5 1 0.5 1 0.5 1 0.5 1 0.5 1 0.5 
+1.00 1.000 0.000 0 
+0.000 0 
+0.000 0.000 0.000 
+1.0,
+treble=g=5,
+equalizer=f=150:width_type=h:width=100:g=3,
+equalizer=f=800:width_type=h:width=100:g=-3,
+equalizer=f=5000:width_type=h:width=100:g=3,
+firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
+firequalizer=gain_entry='entry(-10,0);entry(10,2)',
+ladspa=sc4_1882:plugin=sc4:c=0.5 50 100 -10 5 1 10,
 loudnorm=I=-16:LRA=11:TP=-1.5:print_format=summary,
-aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,\
-aresample=resampler=soxr:osf=s16[enhanced];\
+aecho=0.8:0.9:75:0.255,
 
-[1:a]loudnorm=I=-16:LRA=11:TP=-1.5:print_format=summary,\
-aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,\
-aresample=resampler=soxr:osf=s16[audio];\
+aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
+aresample=resampler=soxr:osf=s16[enhanced];
+[1:a]
+aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
+aresample=resampler=soxr:osf=s16,
+loudnorm=I=-16:LRA=11:TP=-1.5:print_format=summary[audio];
+[audio][enhanced]amix=inputs=2:weights=0.4|0.6;
+" -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav;
 
-[audio][enhanced]amix=inputs=2:weights=0.4|0.6;\
-" ${1}_go.wav -y;
 
 
 #lv2=p='urn\\:jeremy.salwen\\:plugins\\:talentedhack':c=mix=1.00|\
