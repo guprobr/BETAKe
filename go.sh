4728dc0dcb5a2984ac37c9d99c55e992c7323f68
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-12 19:20:27 -0300

    mega upgrade

--- go.sh
+++ go.sh
@@ -3,9 +3,8 @@
 pactl unload-module module-loopback;
 pactl unload-module ladspa-sink;
 
-
-ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
-[0:a]
+ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "     
+[0:a] 
 adeclip,
 anlmdn=s=55,
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
@@ -15,15 +14,16 @@ c=440 0 0
 0 0 0 0 0 0 0 0 0 0 0 0 
 1 1 0 0 
 0.000 0 
-0.000 0.000 0.000 
+0.000 0.000 0.000   
 1.0,
+aecho=0.8:0.9:69:0.3,
 treble=g=5,
 equalizer=f=150:width_type=h:width=100:g=3,
-equalizer=f=800:width_type=h:width=100:g=-3,
-equalizer=f=5000:width_type=h:width=100:g=3,
-ladspa=tap_deesser:plugin=tap_deesser:c=-20 2000 1 1,
-ladspa=fast_lookahead_limiter_1913:plugin=fastLookaheadLimiter:c=-20 -3 0.1,
-ladspa=sc4_1882:plugin=sc4:c=0.5 50 100 -10 5 1 10,
+equalizer=f=800:width_type=h:width=100:g=-3,      
+equalizer=f=5000:width_type=h:width=100:g=3,      
+afftdn=nr=12:nf=-50:nt=w:om=o:adaptivity=0.5:floor_offset=1.0:band_multiplier=1.25,
+ladspa=fast_lookahead_limiter_1913:plugin=fastLookaheadLimiter:c=-3 -3 0.1,
+ladspa=sc4_1882:plugin=sc4:c=0.5 50 100 -20 10 5 12,
 loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[enhanced];
