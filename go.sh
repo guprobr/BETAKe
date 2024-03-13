0cb489b3eaa64b962da6fa1694c4628c18bf6ea6
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-13 16:35:27 -0300

    v1.7
    more natural autotune
    better masterization

--- go.sh
+++ go.sh
@@ -13,8 +13,9 @@ adeclip,
 anlmdn=s=69,
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
 ladspa=tap_autotalent:plugin=autotalent:
-c=440 1.6726875 0.03 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 0.55 1.00 0 0 0 0.33825 1.000 1.000 1 1 0.0 0.20,
+c=440 1.6726875 0.03 0 0 0 0 0 0 0 0 0 0 0 0 1.00 1.00 0 0 0 0.33825 1.000 1.000 0 0 0.0 1.00,
 afftdn,
+treble=g=5,
 equalizer=f=150:width_type=h:width=100:g=3,
 equalizer=f=800:width_type=h:width=100:g=-3,      
 equalizer=f=5000:width_type=h:width=100:g=3,      
@@ -22,20 +23,20 @@ firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
 firequalizer=gain_entry='entry(-10,0);entry(10,2)',
 ladspa=fast_lookahead_limiter_1913:plugin=fastLookaheadLimiter:c=-3 -3 0.1,
 ladspa=sc4_1882:plugin=sc4:c=0.5 50 100 -20 10 5 12,
-treble=g=5,
-aecho=0.8:0.7:300:0.05
+aecho=0.8:0.7:84:0.2
 [voc_enhanced];
 
 [voc_enhanced]
-loudnorm=I=-16:LRA=11:TP=-1.5,
+loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=3dB,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
 
 [1:a]
-loudnorm=I=-16:LRA=11:TP=-1.5,
+loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=4dB,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2:weights=0.4|0.6;
+[play_master][voc_master]amix=inputs=2;
+
 " -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
 
