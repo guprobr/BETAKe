fc6e073f5ecfe8e578b571ce455d2b0508cb49a3
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-13 06:56:50 -0300

    v1.69D

--- go.sh
+++ go.sh
@@ -3,15 +3,18 @@
 pactl unload-module module-loopback;
 pactl unload-module ladspa-sink;
 
+#provide preprocessing for Autotalent, 
+#then, enhance pitch corrected vocal with effects
+#then MASTERIZE for streaming both playback and enhanced vocals, mixing both
+#
 ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
 [0:a]
 adeclip,
 anlmdn=s=69,
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
 ladspa=tap_autotalent:plugin=autotalent:
-c=440 -0.6726875 0 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 0.45 0.98 0 0 0 0.33825 1.000 1.000 0.0 0.0 0.0 1.00,
-deesser=s=0.69,
-compand=attacks=0.3:decays=1:points=-90/-90|-70/-70|-30/-30|-20/-0:soft-knee=0.01,
+c=440 1.6726875 0.03 0 -1 0 -1 0 0 -1 0 -1 0 -1 0 0.55 1.00 0 0 0 0.33825 1.000 1.000 1 1 0.0 0.20,
+afftdn,
 equalizer=f=150:width_type=h:width=100:g=3,
 equalizer=f=800:width_type=h:width=100:g=-3,      
 equalizer=f=5000:width_type=h:width=100:g=3,      
@@ -19,11 +22,11 @@ firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
 firequalizer=gain_entry='entry(-10,0);entry(10,2)',
 ladspa=fast_lookahead_limiter_1913:plugin=fastLookaheadLimiter:c=-3 -3 0.1,
 ladspa=sc4_1882:plugin=sc4:c=0.5 50 100 -20 10 5 12,
-aecho=0.8:0.7:128:0.2222
+treble=g=8,
+aecho=0.8:0.7:300:0.05
 [voc_enhanced];
 
 [voc_enhanced]
-treble=g=5,
 loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
@@ -32,5 +35,6 @@ aresample=resampler=soxr:osf=s16[voc_master];
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2:weights=0.6|0.4;
+[play_master][voc_master]amix=inputs=2:weights=0.4|0.6;
 " -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
+
