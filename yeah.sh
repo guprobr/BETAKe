7ddc2fe4206f4c13190e3303b0233fecc340376b
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 02:56:42 -0300

    yeah v2.0

--- yeah.sh
+++ yeah.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
+# Introducing BETAKê v2.0 main feature: live-processing!
+
+pactl unload-module module-ladspa-sink
+pactl unload-module module-loopback
+pactl unload-module module-echo-cancel
+
+SINKA="echo-cancel-sink";
+
+echo module-echo-cancel;
+pactl load-module module-echo-cancel aec_method=webrtc;
+
+echo fastLookaheadLimite;
+pactl load-module module-ladspa-sink sink_name=ladspa_limiter  plugin=fast_lookahead_limiter_1913  label=fastLookaheadLimiter control="10,1,1000";
+pactl move-sink-input  $( pactl list sink-inputs | grep -i ladspa_limiter -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+echo tap_autotalent;
+pactl load-module module-ladspa-sink sink_name=ladspa_talent plugin="tap_autotalent" control="440,1.6726875,0.0003,\
+        0,0,0,0,0,0,0,0,0,0,0,0,\
+        0.25,1.00,0,0,0,\
+        0.33825,1.000,1.000,\
+        0,0,0.0,0.15" label=autotalent;
+
+        pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_talent -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+
+echo tap_dynamics_m;
+pactl load-module module-ladspa-sink sink_name=ladspa_dyna label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13;
+pactl move-sink-input  $( pactl list sink-inputs | grep -i ladspa_dyna -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+echo tap_equalizer;
+pactl load-module module-ladspa-sink sink_name=ladspa_equal label=tap_equalizer plugin=tap_eq control=-6,-6,-3,0,0,0,0,0,100,200,400,1000,3000,6000,12000,15000;
+pactl move-sink-input  $( pactl list sink-inputs | grep -i ladspa_equal -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+#pactl set-default-sink ladspa_sink;
+#audacity ${1}; #if u feel like editing not recording one-shot
 
 pactl unload-module module-loopback
-pactl unload-module module-ladspa-sink
-
-#echo B
-#EQ and Dynamics
-#pactl load-module module-ladspa-sink sink_name=ladspa_output label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13
-#pactl load-module module-ladspa-sink sink_name=ladspa_output label=tap_equalizer plugin=tap_eq control=-6,-6,-3,0,0,0,0,0,100,200,400,1000,3000,6000,12000,15000; 
-#echo C
-#autotalent 
-#pactl load-module module-ladspa-sink sink_name=ladspa_output master=alsa_output.pci-0000_00_1f.3.analog-stereo.monitor label=autotalent plugin=tap_autotalent control="444,-2,0.0069,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-0.69,-3,0.13,0.21,1,1,0,0,0,0.44";
-
-pactl load-module module-loopback latency_msec=13;
+pactl load-module module-loopback;
 
 aplay ${1}.wav &
 sox  -d ${1}_voc.wav
