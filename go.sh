391702ca873c6a4d33746882568094e5e4ec5abf
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 11:22:28 -0300

    more performance, less redundant filters

--- go.sh
+++ go.sh
@@ -11,29 +11,25 @@ pactl unload-module module-echo-cancel
 ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
 [0:a]
 adeclip,
-anlmdn=s=13,
-afftdn,
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
 treble=g=5,
 equalizer=f=150:width_type=h:width=100:g=3,
 equalizer=f=800:width_type=h:width=100:g=-3,
 equalizer=f=5000:width_type=h:width=100:g=3,
-firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
-firequalizer=gain_entry='entry(-10,0);entry(10,2)',
-aecho=0.8:0.7:100:0.2
+aecho=0.8:0.7:100:0.2222
 [voc_enhanced];
 
 [voc_enhanced]
-loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=4dB,
+loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
 
 [1:a]
-loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=3dB,
+loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2;
+[play_master][voc_master]amix=inputs=2:weights=0.4|0.6;
 
 " -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
 
