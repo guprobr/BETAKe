cdc8a09675562afa9d7b8e2f8fd922e008eb2a25
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 17:19:30 -0300

    v2.1

--- go.sh
+++ go.sh
@@ -8,27 +8,24 @@ pactl unload-module module-echo-cancel
 #we just have to enhance already pitch corrected vocal with effects
 #then MASTERIZE for streaming both playback and enhanced vocals, mixing both
 #
-ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
-[0:a]
-anlmdn,
-ladspa=sc4m_1916:plugin=sc4m:c=0 1.5 2 0 1 1 1,equalizer=f=150:width_type=h:width=100:g=3,
-equalizer=f=800:width_type=h:width=100:g=-3,
-equalizer=f=5000:width_type=h:width=100:g=3,
+ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex 
+"[0:a]
+anlmdn=s=555,
+compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
+firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
+firequalizer=gain_entry='entry(-10,0);entry(10,2)',
+aecho=0.8:0.7:69:0.3,
+acompressor=threshold=-20dB:ratio=2:attack=5:release=200:makeup=6,
 treble=g=5,
-afftdn,aecho=0.8:0.9:100:0.3
-[voc_enhanced];
-
-[voc_enhanced]
-loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=5dB,
+loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
 
 [1:a]
-loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=3dB,
+loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2:weights=0.5|0.6;
-
-" -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
+[play_master][voc_master]amix=inputs=2;"
+ -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
 
