9bba4403c087f9a18ffde4a21b4869b86cbb4170
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 11:55:53 -0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -10,26 +10,27 @@ pactl unload-module module-echo-cancel
 #
 ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
 [0:a]
-afftdn,
-compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
-treble=g=5,
+anlmdn,
+acompressor=threshold=-20dB:ratio=3:attack=50:release=100,
 equalizer=f=150:width_type=h:width=100:g=3,
 equalizer=f=800:width_type=h:width=100:g=-3,
 equalizer=f=5000:width_type=h:width=100:g=3,
-aecho=0.8:0.7:100:0.2222
+ladspa=sc4m_1916:plugin=sc4m:c=0 1.5 2 0 1 1 1,
+treble=g=5,
+afftdn,aecho=0.8:0.9:100:0.3
 [voc_enhanced];
 
 [voc_enhanced]
-loudnorm=I=-16:LRA=11:TP=-1.5,
+loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=5dB,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
 
 [1:a]
-loudnorm=I=-16:LRA=11:TP=-1.5,
+loudnorm=I=-16:LRA=11:TP=-1.5,volume=volume=3dB,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2:weights=0.4|0.6;
+[play_master][voc_master]amix=inputs=2:weights=0.5|0.6;
 
 " -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
 
