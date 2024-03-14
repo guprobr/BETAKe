3cd0aeeefe942fe6ac7d500965294f6e2b16617c
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 11:36:53 -0300

    experimenting with no declipper

--- go.sh
+++ go.sh
@@ -10,7 +10,6 @@ pactl unload-module module-echo-cancel
 #
 ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
 [0:a]
-adeclip,
 afftdn,
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
 treble=g=5,
