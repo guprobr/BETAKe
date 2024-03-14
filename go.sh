855b3f2e1abf1db3c8509d669b234f6f1b2a4cfd
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 17:59:45 -0300

    v2.1 - declipper in better place, no weigths for mix

--- go.sh
+++ go.sh
@@ -9,22 +9,20 @@ pactl unload-module module-echo-cancel
 #then MASTERIZE for streaming both playback and enhanced vocals, mixing both
 #ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
 [0:a]
-adeclip,
 anlmdn=s=33,
 firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
 firequalizer=gain_entry='entry(-10,0);entry(10,2)',
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
-speechnorm,aecho=0.8:0.9:45:0.3,
-loudnorm=I=-16:LRA=11:TP=-1.5,
+speechnorm,afftdn,aecho=0.8:0.9:45:0.3,
+adeclip,loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
 
 [1:a]
-adeclip,
-loudnorm=I=-16:LRA=11:TP=-1.5,
+adeclip,loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2:weights=0.4|0.6;
+[play_master][voc_master]amix=inputs=2;
 " -ar 44100 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
 
