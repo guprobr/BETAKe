194e4e9110f4d72b2ebdc6985e64eac704d76f1d
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 17:20:08 -0300

    v2.1 - live Action

--- yeah.sh
+++ yeah.sh
@@ -9,11 +9,32 @@ pactl unload-module module-echo-cancel
 SINKA="echo-cancel-sink";
 
 echo module-echo-cancel;
- pactl load-module module-echo-cancel aec_method=webrtc;
+pactl load-module module-echo-cancel source_master=alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.mono-fallback aec_method=webrtc;
 
 echo tap_autotalent;
- pactl load-module module-ladspa-sink sink_name=ladspa_talent plugin="tap_autotalent" control="440,1.6726875,0.0003,0,0,0,0,0,0,0,0,0,0,0,0,0.25,1.00,0,0,0,0.33825,1.000,1.000,0,0,0.0,0.15" label=autotalent;
- pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_talent -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+pactl load-module module-ladspa-sink sink_name=ladspa_talent plugin="tap_autotalent" control="440,1.6726875,0.0003,0,0,0,0,0,0,0,0,0,0,0,0,0.25,1.00,0,0,0,0.33825,1.000,1.000,0,0,0.0,0.15" label=autotalent;
+pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_talent -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+echo DYNAMICS
+pactl load-module module-ladspa-sink sink_name=ladspa_dyna label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13
+pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_dyna -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+echo tap_pitch;
+pactl load-module module-ladspa-sink sink_name=ladspa_pitch plugin="tap_pitch" label=tap_pitch control="0,0,0,-3,-3";
+pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_pitch -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+echo fastLookaheadLimiter;
+pactl load-module module-ladspa-sink sink_name=ladspa_limiter plugin="fast_lookahead_limiter_1913" label=fastLookaheadLimiter;
+pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_limiter -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+echo EQUALIZER
+pactl load-module module-ladspa-sink sink_name=ladspa_equalize label=tap_equalizer plugin=tap_eq control=-6,-6,-3,0,0,0,0,0,100,200,400,1000,3000,6000,12000,15000;
+pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_equalize -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+
+
+echo module-declipper;
+pactl load-module module-ladspa-sink sink_name=ladspa_declipper plugin="declip_1195" label=declip;
+pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_declipper -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
 
 #pactl set-default-sink ladspa_sink;
 #audacity ${1}; #if u feel like editing not recording one-shot
