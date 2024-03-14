ed59766ca66d3202645357483122ef724d49cc36
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 17:48:51 -0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -7,19 +7,20 @@ pactl unload-module module-echo-cancel
 #Now on v2.0 live-processing for Autotalent,
 #we just have to enhance already pitch corrected vocal with effects
 #then MASTERIZE for streaming both playback and enhanced vocals, mixing both
-#
-ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
+#ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
 [0:a]
+adeclip,
 anlmdn=s=33,
 firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
 firequalizer=gain_entry='entry(-10,0);entry(10,2)',
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
-speechnorm,aecho=0.8:0.9:1:0.3,
+speechnorm,aecho=0.8:0.9:45:0.3,
 loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[voc_master];
 
 [1:a]
+adeclip,
 loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
