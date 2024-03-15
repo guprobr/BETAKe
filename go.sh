18f4f8598a62750022404751ecbbbc4973956875
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-14 22:31:09 -0300

    v2.1 rdy

--- go.sh
+++ go.sh
@@ -8,7 +8,7 @@ pactl unload-module module-echo-cancel
 #then MASTERIZE for streaming both playback and enhanced vocals, mixing both
 #
 ffmpeg -y -hide_banner -i ${1}_voc.wav -i ${1}.wav -filter_complex "
-[0:a]lv2=p=https\\\\://github.com/lucianodato/noise-repellent:c=reduction=48,
+[0:a]lv2=p=https\\\\://github.com/lucianodato/noise-repellent:c=reduction=13,
 compand=points=-80/-105|-62/-80|-15.4/-15.4|0/-12|20/-7,
 firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
 firequalizer=gain_entry='entry(-10,0);entry(10,2)',
@@ -23,7 +23,7 @@ loudnorm=I=-16:LRA=11:TP=-1.5,
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
 aresample=resampler=soxr:osf=s16[play_master];
 
-[play_master][voc_master]amix=inputs=2,
+[play_master][voc_master]amix=inputs=2:weights=0.4|0.6,
 afade=t=in:st=0:d=4;" -ar 44100 \
                                 ${1}_go.wav && mplayer ${1}_go.wav; #then PLAY!
 
