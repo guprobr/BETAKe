02b88a694de0c21168d884b9c639d8eb15ec5324
Guzpido <gustavo.conte@agenciamop.com.br>
2024-03-16 14:01:41 -0300

    v2.5 final adjusted

--- betaREC.sh
+++ betaREC.sh
@@ -22,19 +22,19 @@ pactl load-module module-alsa-source device=$(pactl list short sources | grep al
 # Load the echo cancellation module to cancel echo from the loopback
 echo "Load module-echo-cancel"
 pactl load-module module-echo-cancel sink_name=echoe master=${SINKA} \
-        aec_method=webrtc aec_args="analog_gain_control=0 digital_gain_control=0";
+        aec_method=webrtc aec_args="analog_gain_control=1 digital_gain_control=1";
 # Load Ladspa effects
 echo "Load module-ladspa-sink for declipper"
 pactl load-module module-ladspa-sink sink_name=ladspa_declipper plugin="declip_1195" label=declip master=echoe;
 echo "Load module-ladspa-sink for pitch"
-pactl load-module module-ladspa-sink sink_name=ladspa_pitch plugin="tap_pitch" label=tap_pitch control="0,0,0,-8,-8" master=ladspa_declipper;
+pactl load-module module-ladspa-sink sink_name=${SINKB} plugin="tap_pitch" label=tap_pitch control="0,0,0,-6,-6" master=ladspa_declipper;
 ### never sactiscatory made autotune to werk live-recording
 #echo "Load module-ladspa-sink for autotalent"
 #pactl load-module module-ladspa-sink sink_name=ladspa_talent plugin="tap_autotalent" control="440,1.6726875,0.003,0,0,0,0,0,0,0,0,0,0,0,0,1.00,1.00,0,0,0,0.33825,1.000,1.000,1,1,0.0,1.00" label=autotalent master=ladspa_pitch;
-echo "Load module-ladspa-sink for dynamics"
-pactl load-module module-ladspa-sink sink_name=ladspa_dyna label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13 master=ladspa_talent;
-echo "Load module-ladspa-sink for lookahead limiter"
-pactl load-module module-ladspa-sink sink_name=${SINKB} plugin="fast_lookahead_limiter_1913" label=fastLookaheadLimiter master=ladspa_dyna;
+#echo "Load module-ladspa-sink for dynamics"
+#pactl load-module module-ladspa-sink sink_name=ladspa_dyna label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13 master=ladspa_talent;
+#echo "Load module-ladspa-sink for lookahead limiter"
+#pactl load-module module-ladspa-sink sink_name=${SINKB} plugin="fast_lookahead_limiter_1913" label=fastLookaheadLimiter master=ladspa_dyna;
 
 echo "sleep 1sec"; sleep 1;
 pactl load-module module-loopback;
