2863aaf11e8d159a56e4eccec28dacc370220bf3
guprobr <40776097+guprobr@users.noreply.github.com>
2024-03-16 12:38:54 -0300

    Update yeah.sh

--- yeah.sh
+++ yeah.sh
@@ -1,45 +1,58 @@
 #!/bin/bash
 
-# Introducing BETAKê v2.0 main feature: live-processing!
-
+# Unload existing modules and restart PulseAudio
 pactl unload-module module-ladspa-sink
 pactl unload-module module-loopback
 pactl unload-module module-echo-cancel
+killall -HUP pipewire-pulse
 
-SINKA="echo-cancel-sink"; SRC_MASTER="alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.mono-fallback"; #adjust according to your cfg ( pactl list sources )
+# Load configuration variables
+source ./config
+echo "SinkA: ${SINKA}"
 
-echo module-echo-cancel;
-pactl load-module module-echo-cancel;
+# Load the null sink module to create a virtual sink named "loopback"
+echo "Wait 2 seconds to initialize..."
+sleep 2;
 
-echo tap_autotalent;
-pactl load-module module-ladspa-sink sink_name=ladspa_talent plugin="tap_autotalent" control="440,1.6726875,0.0003,0,0,0,0,0,0,0,0,0,0,0,0,0.25,1.00,0,0,0,0.33825,1.000,1.000,0,0,0.0,0.15" label=autotalent;
-pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_talent -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+pactl load-module module-alsa-source device=$(pactl list short sources | grep alsa_input | head -n1 | awk '{ print $2 }') sink_name=${SINKA}
 
-echo DYNAMICS
-pactl load-module module-ladspa-sink sink_name=ladspa_dyna label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13
-pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_dyna -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
+# Load the echo cancellation module to cancel echo from the loopback
+echo "Load module-echo-cancel"
+pactl load-module module-echo-cancel sink_name=echoe master=${SINKA} \
+        aec_method=webrtc aec_args="analog_gain_control=0 digital_gain_control=0";
+# Load Ladspa effects
+echo "Load module-ladspa-sink for declipper"
+pactl load-module module-ladspa-sink sink_name=ladspa_declipper plugin="declip_1195" label=declip master=echoe;
+echo "Load module-ladspa-sink for pitch"
+pactl load-module module-ladspa-sink sink_name=ladspa_pitch plugin="tap_pitch" label=tap_pitch control="0,0,0,-8,-8" master=ladspa_declipper;
+### never sactiscatory made autotune to werk live-recording
+#echo "Load module-ladspa-sink for autotalent"
+#pactl load-module module-ladspa-sink sink_name=ladspa_talent plugin="tap_autotalent" control="440,1.6726875,0.003,0,0,0,0,0,0,0,0,0,0,0,0,1.00,1.00,0,0,0,0.33825,1.000,1.000,1,1,0.0,1.00" label=autotalent master=ladspa_pitch;
+echo "Load module-ladspa-sink for dynamics"
+pactl load-module module-ladspa-sink sink_name=ladspa_dyna label=tap_dynamics_m plugin=tap_dynamics_m control=4,700,15,15,13 master=ladspa_talent;
+echo "Load module-ladspa-sink for lookahead limiter"
+pactl load-module module-ladspa-sink sink_name=${SINKB} plugin="fast_lookahead_limiter_1913" label=fastLookaheadLimiter master=ladspa_dyna;
 
-echo tap_pitch;
-pactl load-module module-ladspa-sink sink_name=ladspa_pitch plugin="tap_pitch" label=tap_pitch control="0,0,0,0,0";
-pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_pitch -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
-
-echo fastLookaheadLimiter;
-pactl load-module module-ladspa-sink sink_name=ladspa_limiter plugin="fast_lookahead_limiter_1913" label=fastLookaheadLimiter;
-pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_limiter -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
-
-echo EQUALIZER
-pactl load-module module-ladspa-sink sink_name=ladspa_equalize label=tap_equalizer plugin=tap_eq control=-6,-6,-3,0,0,0,0,0,100,200,400,1000,3000,6000,12000,15000;
-pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_equalize -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
-
-
-echo module-declipper;
-pactl load-module module-ladspa-sink sink_name=ladspa_declipper plugin="declip_1195" label=declip;
-pactl move-sink-input  $( pactl list sink-inputs | grep ladspa_declipper -B25 | grep Sink\ Input | awk '{ print $3 }' | sed 's/#//g' )  ${SINKA};
-
-#pactl set-default-sink ladspa_sink;
-#audacity ${1}; #if u feel like editing not recording one-shot
-
+echo "sleep 3sec"; sleep 1;
 pactl load-module module-loopback;
+
+echo "Starting record audio in 3sec"; sleep 1;
+# Record the audio with effects applied
+echo "Recording audio with effects applied..."
+aplay ${1}.wav &  # Start playback
+parec --device=${SINKB} | sox -t raw -r 44100 -b 16 -c 2 -e signed-integer - -t wav ${1}_voc.wav; 
+
+# Unload existing modules and restart PulseAudio
+pactl unload-module module-ladspa-sink
+pactl unload-module module-loopback
+pactl unload-module module-echo-cancel
+killall -HUP pipewire-pulse
+## kill playback if it is playing
+killall -9 aplay
+
+## trigger post processing
+./betaKE.sh ${1} ${2}
+
 
 aplay ${1}.wav &
 sox  -d ${1}_voc.wav
