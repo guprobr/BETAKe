782864836ba76002de7c3c9956b311e2ea0e0880
Guzpido <gustavo.conte@agenciamop.com.br>
2024-03-25 05:55:30 -0300

    more postprod improvements

--- loop.sh
+++ loop.sh
@@ -6,14 +6,16 @@ OUT_VOCAL=./outputs/BETAKE_out.wav;
 PLAYBACK_BETA=./recordings/BETAKE_playback.mp4;
 PLAYBACK_LEN=$( cat ./outputs/BETAKE_dur.txt);
 diff_ss=0;
-#,
+LC_ALL=C
+
+
 ffmpeg -y -hide_banner -loglevel info  \
-    -ss "$( echo "scale=4; ${diff_ss} - 0.4444 " | bc | sed 's/-\./-0\./g')"    -i "${OUT_VIDEO}" \
-    -ss "$( echo "scale=4; ${diff_ss} - 0.4444 " | bc | sed 's/-\./-0\./g')"    -i "${PLAYBACK_BETA}" \
-                                                                                -i "${OUT_VOCAL}" \
-          -filter_complex "
-   [2:a]adeclip,afftdn,alimiter,dynaudnorm, 
-      ladspa=tap_pitch:plugin=tap_pitch:c=1.0001 21 -20 1,
+    -ss "$( printf "%0.4f" "$( echo "scale=4; 0.4444 + ${diff_ss}  " | bc )" )"  -i "${OUT_VIDEO}" \
+    -ss "$( printf "%0.4f" "$( echo "scale=4; 0.4444 + ${diff_ss}  " | bc )" )" -i "${PLAYBACK_BETA}" \
+                                                                                 -i "${OUT_VOCAL}" \
+            -filter_complex "
+   [2:a]adeclip,afftdn,alimiter,speechnorm,acompressor, 
+      ladspa=tap_pitch:plugin=tap_pitch:c=1.00669 21 -20 15,
       lv2=p='urn\\:jeremy.salwen\\:plugins\\:talentedhack':c=mix=1.0|voiced_threshold=0.99|pitchpull_amount=0.0|pitchsmooth_amount=1.00|mpm_k=1.0|da=0.000|daa=0.000|db=0.000|dc=0.000|dcc=0.000|dd=0.000|ddd=0.000|de=0.000|df=0.000|dff=0.000|dg=0.000|dgg=0.000|oa=0.000|oaa=0.000|ob=0.000|oc=0.000|occ=0.000|od=0.000|odd=0.000|oe=0.000|of=0.000|off=0.000|og=0.000|ogg=0.000|lfo_quant=0.0|lfo_amp=0.0,
      aecho=0.98:0.84:69:0.45,adynamicequalizer,treble=g=5,
        aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
@@ -24,17 +26,23 @@ ffmpeg -y -hide_banner -loglevel info  \
     aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
     aresample=resampler=soxr:osf=s16[playback];
 
-    [playback][vocals]amix=inputs=2:weights=0.5|0.5,
+    [playback][vocals]amix=inputs=2,stereowiden,
     aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
     aresample=resampler=soxr:precision=28;
 
-        [1:v]scale=s=320x180[v1];
-       gradients=n=4:type=circular:s=320x180[spats];
-        gradients=n=3:type=spiral:s=320x180[vscope];
-          [0:v]tile=layout=2x2,scale=s=640x480,colorize[v0]; 
-          [vscope][v1]hstack=inputs=2[video_merge];
-          [video_merge][spats]xstack=inputs=2,colorchannelmixer=aa=0.64,tile=layout=2x1,scale=s=640x480[badcoffee];
-          [v0][badcoffee]overlay;" \
+        [1:v]scale=s=300x200[v1];
+       gradients=n=7:type=circular:s=300x200,scale=s=300x200[spats];
+        gradients=n=6:type=spiral:s=300x200[vscope];
+        [spats][vscope]hstack=inputs=2[spatscope];
+          [1:a]avectorscope,frei0r=dither,colorize=hue=$((RANDOM%361)):saturation=$(bc <<< "scale=2; $RANDOM/32767"):lightness=$(bc <<< "scale=2; $RANDOM/32767"),
+          scale=s=300x200[frodo];
+          [0:v]colorize=hue=$((RANDOM%361)):saturation=$(bc <<< "scale=2; $RANDOM/32767"):lightness=$(bc <<< "scale=2; $RANDOM/32767"),
+          scale=s=1280x720[v0]; 
+          [spatscope]scale=s=300x200[scopy];
+          [v1][scopy]xstack=inputs=2[video_merge];
+          [frodo][video_merge]xstack=inputs=2,scale=s=1280x720[badcoffee];
+          
+          [v0][badcoffee]vstack=inputs=2;" \
              -ar 48000 -t "${PLAYBACK_LEN}" \
      -c:v libx264 -movflags faststart -preset ultrafast  \
        -c:a aac \
