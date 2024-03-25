c3518e0de387c6df414dca62166fc5951cde3f99
Guzpido <gustavo.conte@agenciamop.com.br>
2024-03-24 23:24:03 -0300

    tweak post-prod

--- loop.sh
+++ loop.sh
@@ -7,35 +7,35 @@ PLAYBACK_BETA=./recordings/BETAKE_playback.mp4;
 PLAYBACK_LEN=$( cat ./outputs/BETAKE_dur.txt);
 diff_ss=0;
 
-ffmpeg -y -hide_banner -loglevel info   \
+ffmpeg -y -hide_banner -loglevel info -hwaccel auto  \
     -ss "$( echo "scale=4; ${diff_ss} - 0.4444 " | bc | sed 's/-\./-0\./g')"    -i "${OUT_VIDEO}" \
     -ss "$( echo "scale=4; ${diff_ss} - 0.4444 " | bc | sed 's/-\./-0\./g')"    -i "${PLAYBACK_BETA}" \
                                                                                 -i "${OUT_VOCAL}" \
           -filter_complex "
     [2:a] 
-      ladspa=tap_pitch:plugin=tap_pitch:c=1.03699 21 -19 1, 
+      ladspa=tap_pitch:plugin=tap_pitch:c=0.98669 33 -15 1, 
      ladspa=tap_autotalent:plugin=autotalent:c=440 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1.00 1.00 0 0 0 0 1.000 1.000 0 0 000.0 1.00,
-     aecho=0.8:0.84:84:0.3,volume=volume=5dB
+     aecho=0.8:0.84:98:0.3,volume=volume=4dB,dynaudnorm,treble=g=5
     [vocals];
 
     [1:a]volume=volume=3dB,
     aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
     aresample=resampler=soxr:osf=s16[playback];
 
-    [playback][vocals]amix=inputs=2:weights=0.3|0.6,
+    [playback][vocals]amix=inputs=2:weights=0.5|0.6,
     aresample=resampler=soxr:precision=28;
 
-        [1:v]scale=s=320x180,colorchannelmixer=aa=0.44[v1];
-       gradients=n=7:type=circular:s=320x180[spats];
-        gradients=n=5:type=spiral:s=320x180[vscope];
-          [0:v]tile=color=0xFF8C00:init_padding=3:layout=2x2,scale=s=1280x720[v0]; 
-          [vscope][v1]hstack=inputs=2,scale=s=320x180[video_merge];
-          [video_merge][spats]vstack=inputs=2,colorchannelmixer=aa=0.77,tile=layout=3x1,scale=s=1280x720[badcoffee];
-          [v0][badcoffee]overlay,scale=s=1920x1080;" \
+        [1:v]scale=s=640x360[v1];
+       gradients=n=4:type=circular:s=640x360[spats];
+        gradients=n=3:type=spiral:s=640x360[vscope];
+          [0:v]scale=s=1280x720,colorize[v0]; 
+          [vscope][v1]xstack=inputs=2[video_merge];
+          [video_merge][spats]xstack=inputs=2,colorchannelmixer=aa=0.77,tile=layout=2x1,scale=s=1280x720[badcoffee];
+          [v0][badcoffee]overlay;" \
              -ar 48000 -t "${PLAYBACK_LEN}" \
-     -c:v libx265 -movflags faststart \
-       -c:a pcm_s16le \
-    "${OUT_FILE}" 
+     -c:v libx264 -movflags faststart -preset ultrafast  \
+       -c:a aac \
+       -s 1920x1080 "${OUT_FILE}" 
 
 
 exit
