3f9e621c16a2440c49132af655f44c4effa47381
Guzpido <gustavo.conte@agenciamop.com.br>
2024-03-27 17:28:51 -0300

    low res, faster videos

--- gammaQ.sh
+++ gammaQ.sh
@@ -305,36 +305,36 @@ ffmpeg -y -hide_banner -loglevel info  \
     -ss "$( printf "%0.4f" "$( echo "scale=4; 1.8444 + ${diff_ss}  " | bc )" )" -i "${PLAYBACK_BETA}" \
                                                                                 -i "${OUT_VOCAL}" \
                                                                                 -filter_complex "
-[2:a]
-afftdn=nr=5,compensationdelay,alimiter,speechnorm,acompressor,
-ladspa=tap_pitch:plugin=tap_pitch:c=0.5 90 -20 20,
-ladspa=tap_autotalent:plugin=autotalent:c=440 1.6726875 0.0000 0 0 0 0 0 0 0 0 0 0 0 0 1.00 1.00 0 0 0 0.33825 0.000 0.000 0 0 000.0 0.33,
-aecho=0.8:0.7:99:0.13,
-aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
-aresample=resampler=soxr:osf=s16
-[vocals];
+    [2:a]
+    afftdn=nr=15,compensationdelay,alimiter,speechnorm,acompressor,
+    ladspa=tap_pitch:plugin=tap_pitch:c=0.5 90 -20 20,
+    ladspa=tap_autotalent:plugin=autotalent:c=440 1.6726875 0.0000 0 0 0 0 0 0 0 0 0 0 0 0 1.00 1.00 0 0 0 0.33825 0.000 0.000 0 0 000.0 0.33,
+    aecho=0.8:0.7:99:0.13,
+    aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
+    aresample=resampler=soxr:osf=s16
+    [vocals];
 
-   [1:a]aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
-    aresample=resampler=soxr:osf=s16[playback];
+    [1:a]aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,
+        aresample=resampler=soxr:osf=s16[playback];
 
-   [playback][vocals]amix=inputs=2:weights=0.551|0.669;
+    [playback][vocals]amix=inputs=2:weights=0.551|0.669;
 
-     [1:v]scale=s=848x480[v1];
-       gradients=n=7:type=circular:s=848x480,scale=s=848x480[spats];
-        gradients=n=6:type=spiral:s=848x480[vscope];
+      [1:v]scale=s=640x360[v1];
+        gradients=n=7:type=circular:s=640x360,scale=s=640x360[spats];
+        gradients=n=6:type=spiral:s=640x360[vscope];
         [spats][vscope]overlay=alpha=0.6[spatscope];
-          [1:a]avectorscope=s=848x480[frodo];
-     [0:v]colorize=hue=$((RANDOM%361)):saturation=$(bc <<< "scale=2; $RANDOM/32767"):lightness=$(bc <<< "scale=2; $RANDOM/32767"),
-          scale=s=848x480[v0]; 
-          [spatscope]scale=s=848x480[scopy];
-          [v1][scopy]hstack,scale=s=848x480[video_merge];
-          [frodo][video_merge]hstack,scale=s=848x480[badcoffee];
+        [1:a]avectorscope=s=640x360[frodo];
+        [0:v]colorize=hue=$((RANDOM%361)):saturation=$(bc <<< "scale=2; $RANDOM/32767"):lightness=$(bc <<< "scale=2; $RANDOM/32767"),
+        scale=s=640x360[v0]; 
+        [spatscope]scale=s=640x360[scopy];
+        [v1][frodo]hstack,scale=s=640x360[video_merge];
+        [scopy][video_merge]hstack,scale=s=640x360[badcoffee];
           
-          [v0][badcoffee]vstack,scale=s=1280x720;" \
+        [v0][badcoffee]vstack,scale=s=1280x720;" \
              -ar 44100 -t "${PLAYBACK_LEN}" \
      -c:v libx264 -movflags faststart  \
        -c:a aac \
-         "${OUT_FILE}" &
+         "${OUT_FILE}"  &
            ff_pid=$!;
 
 FINAL_FILE="${OUT_FILE}";
