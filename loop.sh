3e2fc0ca67c71a0631f0d11806be5b16f56cfff7
Guzpido <gustavo.conte@agenciamop.com.br>
2024-03-27 13:52:32 -0300

    fix postprod noisr reduction

--- loop.sh
+++ loop.sh
@@ -13,31 +13,27 @@ ffmpeg -y -hide_banner -loglevel info  \
     -ss "$( printf "%0.4f" "$( echo "scale=4; 1.8444 + ${diff_ss}  " | bc )" )"  -i "${OUT_VIDEO}" \
     -ss "$( printf "%0.4f" "$( echo "scale=4; 1.8444 + ${diff_ss}  " | bc )" )" -i "${PLAYBACK_BETA}" \
                                                                                  -i "${OUT_VOCAL}" \
-            -filter_complex "[2:a]anlmdn=s=5,ladspa=tap_pitch:plugin=tap_pitch:c=0.5 45 -20 15,acompressor,
+            -filter_complex " 
+    [2:a]anlmdn=s=25,afftdn=nr=25,ladspa=tap_pitch:plugin=tap_pitch:c=0.5 13 -20 19,acompressor,
     ladspa=tap_autotalent:plugin=autotalent:c=440 1.6726875 0.0000 0 0 0 0 0 0 0 0 0 0 0 0 1.000 1.00 0 0 0 0.33825 0.000 0.000 0 0 000.0 1.000,
-    firequalizer=gain_entry='entry(250,-5);entry(4000,3)',
-    firequalizer=gain_entry='entry(-10,0);entry(10,2)',
-    aecho=0.9:0.88:84:0.21,
-    extrastereo=m=2.5,aecho=0.8:0.7:69:0.21,treble=g=5,deesser=i=0.64,
-    volume=volume=8dB,
+    adynamicequalizer,aexciter,aecho=0.8:0.88:84:0.33,
+    treble=g=5,deesser=i=0.64,loudnorm=I=-16:LRA=11:TP=-1.5,
     aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
     aresample=resampler=soxr:osf=s16
     [vocals];
 
-    [1:a]volume=volume=4dB,
+   [1:a]loudnorm=I=-16:LRA=11:TP=-1.5,
     aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
     aresample=resampler=soxr:osf=s16[playback];
 
-    [playback][vocals]amix=inputs=2:weights=0.4|0.6,
-    aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,
-    aresample=resampler=soxr:precision=28;
+   [playback][vocals]amix=inputs=2;
 
-        [1:v]scale=s=300x200[v1];
+     [1:v]scale=s=300x200[v1];
        gradients=n=7:type=circular:s=300x200,scale=s=300x200[spats];
         gradients=n=6:type=spiral:s=300x200[vscope];
         [spats][vscope]overlay=alpha=0.6[spatscope];
           [1:a]avectorscope=s=300x200[frodo];
-          [0:v]colorize=hue=$((RANDOM%361)):saturation=$(bc <<< "scale=2; $RANDOM/32767"):lightness=$(bc <<< "scale=2; $RANDOM/32767"),
+     [0:v]colorize=hue=$((RANDOM%361)):saturation=$(bc <<< "scale=2; $RANDOM/32767"):lightness=$(bc <<< "scale=2; $RANDOM/32767"),
           scale=s=1280x720[v0]; 
           [spatscope]scale=s=300x200[scopy];
           [v1][scopy]xstack=inputs=2[video_merge];
